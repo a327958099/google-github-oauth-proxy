3246ad220d6a1439ded12c30a102479f5339bb84
Mufei <327958099@qq.com>
2024-03-11 11:25:30 +0800

    d

--- pages/api/google/oauth.ts
+++ pages/api/google/oauth.ts
@@ -35,11 +35,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
       jsonRes(res,{data: username});
     } catch (err) {
-        res.status(500).json({name:String(err)});
-    //   jsonRes(res, {
-    //     code: 500,
-    //     error: err
-    //   });
+        // res.status(500).json({name:String(err)});
+      jsonRes(res, {
+        code: 500,
+        error: err
+      });
     }
   }
   
